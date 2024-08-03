cb434f0f2e5b4062db8a4d5eea750f45c4ae8d7b
Trckmazz <dylanprinsloo@icloud.com>
2024-08-03 16:09:48 +0200

    commit

--- Year 1/Semester 2/Java/Lessons/objects.java
+++ Year 1/Semester 2/Java/Lessons/objects.java
@@ -1,4 +1,29 @@
+import java.util.Scanner;
+
 public class objects {
-    
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // sc.next();
+
+        String h = "Hello";
+        h.length();
+
+        second("second method", 4);
+
+        // calling to third
+        System.out.println(third(20));
+
+    } 
+    // void means returns nothing
+    public static void second(String str, int x) {  
+        for (int i = 0; i < x; i++) {
+            System.out.println(str); 
+        }
+        
+    }
+
+    public static int third( int x) {
+        return x + 2;
+        
+    }
 }
