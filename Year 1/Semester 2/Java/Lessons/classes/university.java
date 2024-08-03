d6e0541a355ef0e05948b5bb8c5c602272a4c0b3
Trckmazz <dylanprinsloo@icloud.com>
2024-08-03 16:40:00 +0200

    update

---
+++ Year 1/Semester 2/Java/Lessons/classes/university.java
@@ -0,0 +1,38 @@
+package  classes;
+
+public class university {
+
+    private String name;
+    private int year;
+
+    // constructor method
+
+    public university(String name, int year) {
+        // this is used
+        this.name = name;
+        this.year = year;
+        graduate();
+
+    }
+
+    public void speak() {
+        System.out.println("I am in the " + this.name + "and I am in year " + this.year);
+    }  
+
+    public int getYear() {
+        return this.year();
+    }
+
+    // chnage the university or year?
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    // private method
+    private int graduate() {
+        return this.year + 2;
+    }
+}
+
+// object will be created in Main - file discussion
